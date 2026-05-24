6d941cd487e0f6b399ccae332101b264ac4812d6
Yogesh Gautam <yogeshgautam2006@gmail.com>
2026-05-24 11:15:02 +0530

    Moved to permanent Neon PostgreSQL database

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Hum database ka naam grocery_saas.db rakh rahe hain
-SQLALCHEMY_DATABASE_URL = "sqlite:///./grocery_saas.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:npg_ETwCyAih0R2F@ep-square-union-apcjatep.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require"
 
 # Engine database ke sath connection establish karta hai
 engine = create_engine(
