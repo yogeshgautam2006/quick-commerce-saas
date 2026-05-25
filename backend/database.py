ccd53ab41532210edd600f3d603729aa6e6bcc57
Yogesh Gautam <yogeshgautam2006@gmail.com>
2026-05-25 07:55:26 +0530

    Fixed PostgreSQL connection args

--- backend/database.py
+++ backend/database.py
@@ -2,13 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Hum database ka naam grocery_saas.db rakh rahe hain
+# Aapka Neon PostgreSQL ka URL
 SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:npg_ETwCyAih0R2F@ep-square-union-apcjatep.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require"
 
-# Engine database ke sath connection establish karta hai
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# Engine database ke sath connection establish karta hai (SQLite wali setting hata di gayi hai)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # SessionLocal actual database queries run karne ke kaam aayega
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
